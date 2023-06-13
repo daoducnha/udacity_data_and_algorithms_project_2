b8ae3ae63ce5a6ae138f46af17d8d709e779c4c2
nhadd <aoducnha29493@gmail.com>
2023-06-13 20:19:54 +0700

    add test case for problem 2

--- problem2/File_Recursion.py
+++ problem2/File_Recursion.py
@@ -30,13 +30,21 @@ def find_files(suffix, path):
 
     return list_paths
 
-ls = find_files(".c", "testdir")
-print(ls)
 ## Add your own test cases: include at least three test cases
 ## and two of them must include edge cases, such as null, empty or very large values
 
 ## Test Case 1
-
+print("=============Test case 1=============")
+ls = find_files(".c", "testdir")
+print(ls)
+assert ls == ['testdir/subdir3/subsubdir1/b.c', 'testdir/t1.c', 'testdir/subdir5/a.c', 'testdir/subdir1/a.c']
 ## Test Case 2
-
+print("=============Test case 2=============")
+ls = find_files(".h", "testdir")
+print(ls)
+assert ls == ['testdir/subdir3/subsubdir1/b.h', 'testdir/subdir5/a.h', 'testdir/t1.h', 'testdir/subdir1/a.h']
 ## Test Case 3
+print("=============Test case 3=============")
+ls = find_files(".gitkeep", "testdir")
+print(ls)
+assert ls == ['testdir/subdir4/.gitkeep', 'testdir/subdir2/.gitkeep']
